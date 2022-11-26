377dc3e4111151394239410ee8ff572d57d86180
lukaszkuchta <lukaszkuchta10@gmail.com>
2022-11-26 13:27:21 +0100

    Dodanie buildera do Student

--- src/main/java/pl/sda/hibernate/demo/Main.java
+++ src/main/java/pl/sda/hibernate/demo/Main.java
@@ -17,6 +17,14 @@ public class Main {
             student.setKierunekNauczania("Informatyka");
             student.setIndeks("123123");
 
+            Student strudent2 = Student.builder()
+                    .dataUrodzenia(LocalDate.of(1990, 1, 3))
+                    .kierunekNauczania("Informatyka")
+                    .indeks("123123")
+                    .imie("Pawel")
+                    .build();
+
+
             session.persist(student);
 
             transaction.commit();
--- src/main/java/pl/sda/hibernate/demo/Student.java
+++ src/main/java/pl/sda/hibernate/demo/Student.java
@@ -1,6 +1,8 @@
 package pl.sda.hibernate.demo;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,7 +10,9 @@ import java.time.LocalDate;
 
 @Data
 @Entity
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
