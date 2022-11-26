f610574deb2484efa26e4ca6733060ed90c5fffc
lukaszkuchta <lukaszkuchta10@gmail.com>
2022-11-26 12:42:59 +0100

    Dodanie nowej encji do bazy danych

--- src/main/java/pl/sda/hibernate/demo/Main.java
+++ src/main/java/pl/sda/hibernate/demo/Main.java
@@ -1,10 +1,28 @@
 package pl.sda.hibernate.demo;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.io.IOException;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        Session session = HibarnateUtil.INSTANCE.getSessionFactory().openSession();
-        session.close();
+        try(Session session = HibarnateUtil.INSTANCE.getSessionFactory().openSession()){
+            Transaction transaction = session.beginTransaction();
+
+            Student student = new Student();
+            student.setImie("Pawel");
+            student.setDataUrodzenia(LocalDate.of(1990, 1, 3));
+            student.setKierunekNauczania("Informatyka");
+            student.setIndeks("123123");
+
+            session.persist(student);
+
+            transaction.commit();
+        }catch(Exception ioe){
+
+        }
+
     }
 }
